4b05ed48fb679b203656f53f33eb797f8a9c9a7f
Luihmn <61132305+LHneto@users.noreply.github.com>
2023-06-11 22:07:59 -0300

    questao4.c

---
+++ Avaliações - LLP/Avaliação 3/questao4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char nomeArquivo[100];
+    char linha[100];
+    int contadorVogais = 0;
+    int contadorConsoantes = 0;
+    FILE *arquivo;
+
+    // Solicita o nome do arquivo ao usuário
+    printf("Qual arquivo você deseja abrir? ");
+    scanf("%s", nomeArquivo);
+
+    
+    arquivo = fopen(nomeArquivo, "r");
+
+    if (arquivo == NULL) {
+        printf("Erro.\n");
+        return 1; 
+    }
+
+    
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        for (int i = 0; linha[i] != '\0'; i++) {
+            char caractere = tolower(linha[i]); 
+
+            if (isalpha(caractere)) {
+                if (caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u') {
+                    contadorVogais++;
+                } else {
+                    contadorConsoantes++;
+                }
+            }
+        }
+    }
+
+    
+    fclose(arquivo);
+
+    printf("Este arquivo possui %d vogais e %d consoantes.\n", contadorVogais, contadorConsoantes);
+
+    return 0; 
+}
